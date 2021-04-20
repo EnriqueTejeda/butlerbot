29bfc0b0350d9b9e01fb213b7f5c2aaec056b5d9
SergioAF <sergio.af@outlook.com>
2021-04-20 08:59:07 -0500

    feat: Add Github App to validate Pull Request format

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 	"strconv"
 	"errors"
-	"github.com/google/go-github/github"
+	"github.com/google/go-github/v34/github" // with go modules enabled (GO111MODULE=on or outside GOPATH)
 	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
 	"github.com/bndr/gojenkins"
@@ -16,6 +16,8 @@ import (
 	"gopkg.in/yaml.v2"
 	"fmt"
 	"io/ioutil"
+	"regexp"
+	"github.com/bradleyfalzon/ghinstallation"
 )
 
 type Command struct {
@@ -40,6 +42,9 @@ type Jenkins struct {
 
 type Github struct {
 	Token string `yaml:"token"`
+	AppId int64 `yaml:"appId"`
+	AppInstallation int64 `yaml:"appInstallation"`
+	AppPrivateKey string `yaml:"appPrivateKey"`
 }
 
 type Server struct {
@@ -195,6 +200,64 @@ func handleComments(event *github.IssueCommentEvent, client *github.Client, c *C
 	return nil
 }
 
+func validateRegex(text string, regex string)(bool){
+	var valid = regexp.MustCompile(regex)
+	return valid.MatchString(text)
+}
+
+func validateBody(text string, regex string)(bool){
+	var valid = regexp.MustCompile(regex)
+	substring := valid.FindStringSubmatch(text)
+	if len(substring) > 0 {
+		description := strings.TrimSpace(valid.FindStringSubmatch(text)[1])
+		if len(description) > 0 && description != "Please describe your pull request."{
+			return true
+		}
+	}
+	return false
+}
+
+func createCheck(client *github.Client, owner string, repo string, sha, name string, valid bool){
+	var conclusion = "skipped"
+	if valid == true {
+		conclusion = "success"
+	}else{
+		conclusion = "failure"
+	}
+
+	opts := github.CreateCheckRunOptions{
+		Name:       name,
+		HeadSHA:    sha,
+		Status:     github.String("completed"),
+		Conclusion: github.String(conclusion),
+	 }
+
+	 _, _, err := client.Checks.CreateCheckRun(context.Background(), owner, repo, opts)
+	 if err != nil {
+		log.Error(err)
+	}
+}
+
+func handlePullRequest(event *github.PullRequestEvent, client *github.Client, c *Config) (error) {
+	var (
+		title = event.GetPullRequest().GetTitle()
+		body = event.GetPullRequest().GetBody()
+		owner = event.GetRepo().GetOwner().GetLogin()
+		repo = event.GetRepo().GetName()
+		sha = event.GetPullRequest().Head.GetSHA()
+		regexTitle = `^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(\([a-z ]+\))?(!)?: [\w ]+$`
+		regexBody = `(?s)## Description(.*)## Other information`
+	)
+
+	isValidTitle := validateRegex(title, regexTitle)
+	createCheck(client, owner, repo, sha, "Conventional Commits", isValidTitle)
+
+	isValidBody := validateBody(body,regexBody)
+	createCheck(client, owner, repo, sha, "Description Pull Request", isValidBody)
+
+	return nil 
+}
+
 func newClient(githubToken string) (*github.Client){
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: githubToken},
@@ -203,12 +266,28 @@ func newClient(githubToken string) (*github.Client){
 	return github.NewClient(tc)
 }
 
+func newAppClient(githubConfig *Github)(*github.Client){
+	// Shared transport to reuse TCP connections.
+	tr := http.DefaultTransport
+	key := []byte(githubConfig.AppPrivateKey)
+
+	// Wrap the shared transport for use with the app ID authenticating with installation ID.
+	itr, err := ghinstallation.New(tr, githubConfig.AppId, githubConfig.AppInstallation, key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Use installation transport with github.com/google/go-github
+	return github.NewClient(&http.Client{Transport: itr})
+}
+
 type webhookHandler struct {
 	Config *Config
 }
 
 func (wh *webhookHandler) handler(w http.ResponseWriter, r *http.Request) {
 	githubClient := newClient(wh.Config.Github.Token)
+	githubAppClient := newAppClient(wh.Config.Github)
 	payload, err := github.ValidatePayload(r, []byte(""))
 	if err != nil {
 		log.Error("error validating request body: err=%s\n", err)
@@ -227,6 +306,12 @@ func (wh *webhookHandler) handler(w http.ResponseWriter, r *http.Request) {
 				log.Error(err)
 				return
 			}
+        case *github.PullRequestEvent:
+            err := handlePullRequest(e, githubAppClient, wh.Config)
+            if err != nil {
+                log.Error(err)
+                return
+            }
 		default:
 			return
 	}
